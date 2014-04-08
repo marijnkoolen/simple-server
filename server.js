389c6876f0396909aa2bc78d114ebb4af384d02e
Marijn Koolen <marijn.koolen@gmail.com>
2014-04-08 16:13:18 +0200

    change the port to 3333

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3100)
-console.log("server is listening on port 3100")
+app.listen(3333)
+console.log("server is listening on port 3333")
